f3e8bf0fe3eb355c2d5d364794e61a0704ba4196
Preston Rizzo <150842716+TheGhost454@users.noreply.github.com>
2024-02-22 14:06:39 -0600

    streak_multiplier.cpp and streak_multiplier.h
    corrected code to determine a streak and get a random multiplier between 2 and 5.

---
+++ streak_multiplier.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+/*
+ * C++ Sea Monster game
+ * @filename: streak_multiplier.H
+ * @datetime: Spring 2024
+ * @author: Preston Rizzo
+*/
+
+#ifndef STREAK_MULTIPLIER_H
+#define STREAK_MULTIPLIER_H
+
+void streak(int correct_answers, int starting_damage);
+void multiplier_value(bool multer, int base_damage);
+
+
+
+#endif //STREAK_MULTIPLIER
